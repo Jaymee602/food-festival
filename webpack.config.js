86dede8fba5c171221302d6f9bc5f769572ece2c
jam <jaymee602@gmail.com>
2022-08-11 16:48:05 -0700

    allow offline functionality

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,35 @@
-const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
-
+const webpack = require('webpack');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+// const SWPrecacheWebpackPlugin = require("sw-precache-webpack-plugin");
+// const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require('path');
-const webpack = require("webpack");
 
-module.exports = {
+const config = {
   entry: {
-    app: "./assets/js/script.js",
-    events: "./assets/js/events.js",
-    schedule: "./assets/js/schedule.js",
-    tickets: "./assets/js/tickets.js"
+    app: './assets/js/script.js',
+    events: './assets/js/events.js',
+    schedule: './assets/js/schedule.js',
+    tickets: './assets/js/tickets.js'
   },
   output: {
+    path: path.join(__dirname + "/dist"),
     filename: "[name].bundle.js",
-    path: __dirname + "/dist",
+    publicPath: '/',
   },
   module: {
     rules: [
       {
-        test: /\.jpg$/i,
+        test: /\.(png|jpe?g|gif)$/i,
         use: [
           {
             loader: 'file-loader',
             options: {
               esModule: false,
-              name (file) {
-                return "[path][name].[ext]"
+              name(file) {
+                return '[path][name].[ext]';
               },
-              publicPath: function(url) {
-                return url.replace("../", "/assets/")
+              publicPath(url) {
+                return url.replace('../', '/assets/');
               }
             }
           },
@@ -38,14 +40,23 @@ module.exports = {
       }
     ]
   },
+  devServer: {
+    static: {
+      directory: __dirname,
+    },
+    compress: true,
+    port: 3001,
+  },
   plugins: [
     new webpack.ProvidePlugin({
-        $: "jquery",
-        jQuery: "jquery"
-      }),
+      $: 'jquery',
+      jQuery: 'jquery'
+    }),
     new BundleAnalyzerPlugin({
-        analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+      analyzerMode: 'static'
     })
   ],
   mode: 'development'
 };
+
+module.exports = config;
